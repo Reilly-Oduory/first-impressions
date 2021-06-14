c2525d478dccbd3d24e00c8547acc9e771824972
Reilly-Oduory <reilly.oduory@student.moringaschool.com>
2021-06-14 10:08:39 +0300

    setup

---
+++ config.py
@@ -0,0 +1,16 @@
+from logging import DEBUG
+
+class Config:
+    pass
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
