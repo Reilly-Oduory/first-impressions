1e990ebf04bdbd17d79868abfdf7e5a0e8072db1
Reilly-Oduory <reilly.oduory@student.moringaschool.com>
2021-06-16 21:25:12 +0300

    send email working

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "reilly.oduory@student.moringaschool.com"
-    MAIL_PASSWORD = "BitchIcode"
+    MAIL_USERNAME = 'reilly.oduory@student.moringaschool.com'
+    MAIL_PASSWORD = 'BitchIcode'
 
 class ProdConfig(Config):
     pass
